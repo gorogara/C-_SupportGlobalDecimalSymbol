205562045b83713a38c4dd0486891598cd06c4d9
JO Gyeongmin <gorogara87@gmail.com>
2022-09-09 10:44:09 +0200

    init commit

---
+++ TestDecimal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestDecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "3.7";
+
+            try
+            {
+                var a = float.Parse(input, System.Globalization.NumberStyles.AllowDecimalPoint, System.Globalization.CultureInfo.InvariantCulture);
+                Console.WriteLine(a.ToString(System.Globalization.CultureInfo.GetCultureInfo("en-GB")));
+
+                File.WriteAllText("a.txt", a.ToString(System.Globalization.CultureInfo.GetCultureInfo("en-GB")));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
